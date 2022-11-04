eb6acf77881c0570469e53168b9a8ff8d01b973a
Kasish Seetal <kseetal@rems2.com>
2022-11-04 14:57:37 +0200

    yeet

--- main.py
+++ main.py
@@ -1,64 +1,63 @@
-from flask import Flask, render_template, Response, request, send_file
-import cv2
-import datetime, time
-import os, sys
+import base64
+import datetime
+import os, shutil
 import numpy as np
-from threading import Thread
-
-global capture, switch, rec
-capture = 0
-switch = 1
-rec = 0
+from models.functions import get_image_array, visualize_segmentation, class_colors
+from pathlib import Path
+from flask import Flask, render_template, request, send_file, jsonify
+from project.fcn8 import fcn_8
+from PIL import Image
+import cv2
 
 try:
     os.mkdir('./captures')
+    os.mkdir('./segments')
 except OSError as error:
     pass
 
-# instatiate flask app
+# instantiate flask app
 app = Flask(__name__, template_folder='./templates')
 
-camera = cv2.VideoCapture(0)
-
 IMG_FOLDER = os.path.abspath("captures")
+OUT_FOLDER = os.path.abspath("segments")
 app.config['UPLOAD_FOLDER'] = IMG_FOLDER
-
-def record(out):
-    global rec_frame
-    while (rec):
-        time.sleep(0.05)
-        out.write(rec_frame)
-
-def gen_frames():  # generate frame by frame from camera
-    global out, capture, rec_frame
-    while True:
-        success, frame = camera.read()
-        if success:
-            if capture:
-                capture = 0
-                now = datetime.datetime.now()
-                p = os.path.sep.join(['captures', "capture_{}.png".format(str(now).replace(":", ''))])
-                cv2.imwrite(p, frame)
-
-            try:
-                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame, 1))
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            except Exception as e:
-                pass
-
-        else:
-            pass
 
 
 @app.route('/')
 def index():
+    for filename in os.listdir(IMG_FOLDER):
+        file_path = os.path.join(IMG_FOLDER, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception:
+            pass
+
+    for filename in os.listdir(OUT_FOLDER):
+        file_path = os.path.join(OUT_FOLDER, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            pass
+
     return render_template('index.html')
 
-@app.route('/captured')
+
+@app.route('/captured', methods=['POST'])
 def captured():
-    time.sleep(1)
+
+    file = request.form["image"]
+    imgdata = base64.b64decode(file)
+    now = datetime.datetime.now()
+    p = os.path.sep.join(['captures', "shot_{}.png".format(str(now).replace(":", ''))])
+    with open(p, 'wb') as f:
+        f.write(imgdata)
+
     captures = []
     for file in os.listdir(IMG_FOLDER):
         # check only text files
@@ -67,46 +66,41 @@ def captured():
             captures.append(filepath)
 
     latest = max(captures, key=os.path.getmtime)
-    return send_file(latest, mimetype='image/png')
+    path = Path('fcn_8_resnet50.00005')
+    model = fcn_8(51, input_height=480, input_width=640, channels=3)
+    model.load_weights(path)
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    input_width = model.input_width
+    input_height = model.input_height
+    output_width = model.output_width
+    output_height = model.output_height
+    n_classes = model.n_classes
 
-@app.route('/requests', methods=['POST', 'GET'])
-def tasks():
-    global switch, camera
-    if request.method == 'POST':
-        if request.form.get('click') == 'Capture':
-            global capture
-            capture = 1
-        elif request.form.get('stop') == 'Stop/Start':
+    x = get_image_array(latest, input_width, input_height)
+    pr = model.predict(np.array([x]))[0]
+    pr = pr.reshape((output_height, output_width, n_classes)).argmax(axis=2)
+    inp = cv2.imread(latest, 1)
 
-            if (switch == 1):
-                switch = 0
-                camera.release()
-                cv2.destroyAllWindows()
+    seg_img = visualize_segmentation(pr, inp, n_classes=n_classes,
+                                     colors=class_colors, overlay_img=True,
+                                     prediction_width=output_width,
+                                     prediction_height=output_height)
 
-            else:
-                camera = cv2.VideoCapture(0)
-                switch = 1
-        elif request.form.get('rec') == 'Start/Stop Recording':
-            global rec, out
-            rec = not rec
-            if (rec):
-                now = datetime.datetime.now()
-                fourcc = cv2.VideoWriter_fourcc(*'XVID')
-                out = cv2.VideoWriter('vid_{}.avi'.format(str(now).replace(":", '')), fourcc, 20.0, (640, 480))
-                # Start new thread for recording the video
-                thread = Thread(target=record, args=[out, ])
-                thread.start()
-            elif (rec == False):
-                out.release()
+    out = Image.fromarray(seg_img.astype("uint8"))
+    out.save("./segments/output"+format(str(now).replace(":", ''))+".png", "PNG")
+    segments = []
+    for file in os.listdir(OUT_FOLDER):
+        # check only text files
+        if file.endswith('.png'):
+            filepath = os.path.join(OUT_FOLDER, file)
+            segments.append(filepath)
+    out_seg = max(segments, key=os.path.getmtime)
+    with open(out_seg, "rb") as img_file:
+        img64 = base64.b64encode(img_file.read())
+    base64string = 'data:image/png;base64,' + img64.decode('utf-8')
+    return base64string
 
 
-    elif request.method == 'GET':
-        return render_template('index.html')
-    return render_template('index.html')
-
-camera.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, ssl_context='adhoc')
